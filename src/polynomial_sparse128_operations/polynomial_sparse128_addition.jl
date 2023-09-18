839724cda851492caf57c2b7d4bac7d7d7c8fe77
Samuel Dick <s.dick1@uqconnect.edu.au>
2023-09-18 15:35:07 +1000

    Updated operations. Same changes as to sparse.

--- src/polynomial_sparse128_operations/polynomial_sparse128_addition.jl
+++ src/polynomial_sparse128_operations/polynomial_sparse128_addition.jl
@@ -7,37 +7,46 @@
 ########################################################################
 
 """
-Add a sparse polynomial and a term.
+Add a polynomial (sparse128) and a term.
 """
 function +(p::PolynomialSparse128, t::Term128)
     p = deepcopy(p)
-    if t.degree ∈ [i.degree for i in p.lst] #Term of same degree exists in p
-         for i in 1:length(p.lst)
-            if p.lst[i].degree == t.degree
-                p.lst[i] += t
-            end 
-        end 
-    else #Term of same degree does not exist in p
+    if t.degree ∈ keys(p.dict) #polynomial contains term of same degree
+        add_term = get_element(p.lst, p.dict, t.degree) + t #sum terms of same degree 
+        delete_element!(p.lst, p.dict, t.degree) 
+        push!(p, add_term) #add new term in place of removed term 
+    else 
         push!(p, t)
-    end
-    return trim!(p)
-end
-
-+(t::Term, p::PolynomialSparse128) = p + t
-
-"""
-Add two sparse polynomials.
-"""
-function +(p1::PolynomialSparse128, p2::PolynomialSparse128)::PolynomialSparse128
-    p = deepcopy(p1)
-    for t in p2.lst 
-        p += t
-    end
+    end 
     return p
 end
 
++(t::Term128, p::PolynomialSparse128) = p + t
+
 """
-Add a sparse polynomial and an integer.
+Add two sparse128 polynomials.
 """
-+(p::PolynomialSparse128, n::Int) = p + Term128(Int128(n),0)
-+(n::Int, p::PolynomialSparse128) = p + Term128(Int128(n),0)
+function +(p1::PolynomialSparse128, p2::PolynomialSparse128)::PolynomialSparse128
+    q1 = deepcopy(p1) #pop! and push! mutate polynomials
+    q2 = deepcopy(p2)
+    sum_pol = PolynomialSparse128() #initialise sum of poylnomials 
+    while !iszero(q1) || !iszero(q2)
+        if degree(q1) == degree(q2) #degrees equal, add leading terms
+            ltq1, ltq2 = pop!(q1), pop!(q2)
+            if ltq1 + ltq2 ≠ 0 #PolynomialSparse contains only nonzero terms
+                push!(sum_pol, ltq1 + ltq2)
+            end 
+        elseif degree(q1) > degree(q2)  
+            push!(sum_pol, pop!(q1)) #leading term of q1 added  
+        else 
+            push!(sum_pol, pop!(q2)) #leading term of q2 added
+        end 
+    end 
+    return sum_pol 
+end
+
+"""
+Add a sparse128 polynomial and an integer.
+"""
++(p::PolynomialSparse128, n::Int128) = p + Term128(n,0)
++(n::Int128, p::PolynomialSparse128) = p + Term128(n,0)
--- src/polynomial_sparse128_operations/polynomial_sparse128_multiplication.jl
+++ src/polynomial_sparse128_operations/polynomial_sparse128_multiplication.jl
@@ -10,12 +10,12 @@
 Multiply two sparse polynomials.
 """
 function *(p1::PolynomialSparse128, p2::PolynomialSparse128)::PolynomialSparse128
-    p_out = PolynomialSparse128()
-    for t in p1.lst
-        new_summand = (t * p2)
-        p_out = p_out + new_summand
-    end
-    return p_out
+    p_out = PolynomialSparse128() #initialise result of multiplication
+    p1_terms = collect(p1.lst)
+    for t in p1_terms 
+        p_out += t*p2 #multiply each term of p1 by p2
+    end 
+    return p_out 
 end
 
 """
